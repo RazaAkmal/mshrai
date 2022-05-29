ba27c292abed7a9b0d3034416e69eb4f4abc2b70
Bilal Imrani <bimrani816@gmail.com>
2022-05-29 19:00:05 +0500

    i18n

--- src/App.js
+++ src/App.js
@@ -5,37 +5,60 @@ import Resault from "./pages/resaults";
 import Search from "./pages/search";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import Cookies from 'js-cookie'
-import uniqid from 'uniqid';
+import Cookies from "js-cookie";
+import uniqid from "uniqid";
+import "./i18n";
+import { useTranslation, Trans } from "react-i18next";
+
+const lngs = {
+  ar: { nativeName: "Arabic" },
+  en: { nativeName: "English" },
+};
 
 const App = () => {
-
   useEffect(() => {
-    const getID = Cookies.get('id')
+    const getID = Cookies.get("id");
     if (!getID) {
-      let userId = uniqid('userId-')
-      Cookies.set('id', userId)
+      let userId = uniqid("userId-");
+      Cookies.set("id", userId);
     }
-  }, [])
-  
+  }, []);
 
+  const { t, i18n } = useTranslation();
 
   return (
-    <Provider store={store}>
-    <Router basename="/">
-      <div className="App">
-        <Switch>
-          <Route exact path="/results">
-            <Resault />
-          </Route>
-          <Route exact path="/">
-            <Search />
-          </Route>
-        </Switch>
+    <>
+      <div>
+        {Object.keys(lngs).map((lng) => (
+          <button
+            key={lng}
+            style={{
+              fontWeight: i18n.resolvedLanguage === lng ? "bold" : "normal",
+            }}
+            type="submit"
+            onClick={() => i18n.changeLanguage(lng)}
+          >
+            {lngs[lng].nativeName}
+          </button>
+        ))}
       </div>
-    </Router>
-    </Provider>
+
+      <Provider store={store}>
+        <Router basename="/">
+          <div className="App">
+            <Switch>
+              <Route exact path="/results">
+                <Resault />
+              </Route>
+              <Route exact path="/">
+                <Search />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
+    </>
   );
-}
+};
 
 export default App;
--- src/constants.js
+++ src/constants.js
@@ -1,46 +1,47 @@
 export const colourStyles = {
-    control: (styles) => ({
+  control: (styles) => ({
+    ...styles,
+    backgroundColor: "white",
+    padding: "8px",
+    textAlign: "start",
+    boxShadow: "0 0 0 1px #ddd",
+    ":hover": { borderColor: "#ddd" },
+  }),
+  menu: (styles) => ({
+    ...styles,
+    zIndex: "1000",
+  }),
+  option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+    return {
       ...styles,
-      backgroundColor: "white",
-      padding: "8px",
-      boxShadow: "0 0 0 1px #ddd",
-      ":hover": { borderColor: "#ddd" },
-    }),
-    menu: (styles) => ({
-      ...styles,
-      zIndex: "1000",
-    }),
-    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
-      return {
-        ...styles,
-        backgroundColor: isDisabled
-          ? undefined
-          : isSelected
-          ? data.color
-          : isFocused
-          ? "#ddd"
-          : undefined,
-        color: "black",
-        cursor: isDisabled ? "not-allowed" : "default",
-        ":active": {
-          ...styles[":active"],
-          backgroundColor: !isDisabled ? "#ddd" : undefined,
-        },
-      };
-    },
-    multiValue: (styles, { data }) => {
-      return {
-        ...styles,
-        backgroundColor: "#aaf3ff",
-        color: "#000",
-      };
-    },
-    multiValueLabel: (styles, { data }) => ({
-      ...styles,
-      color: "browen",
-    }),
-    multiValueRemove: (styles, { data }) => ({
-      ...styles,
+      backgroundColor: isDisabled
+        ? undefined
+        : isSelected
+        ? data.color
+        : isFocused
+        ? "#ddd"
+        : undefined,
       color: "black",
-    }),
-  };
+      cursor: isDisabled ? "not-allowed" : "default",
+      ":active": {
+        ...styles[":active"],
+        backgroundColor: !isDisabled ? "#ddd" : undefined,
+      },
+    };
+  },
+  multiValue: (styles, { data }) => {
+    return {
+      ...styles,
+      backgroundColor: "#aaf3ff",
+      color: "#000",
+    };
+  },
+  multiValueLabel: (styles, { data }) => ({
+    ...styles,
+    color: "browen",
+  }),
+  multiValueRemove: (styles, { data }) => ({
+    ...styles,
+    color: "black",
+  }),
+};
