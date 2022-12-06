f0783cd159eff219bb7fe2f1f190ab362c77db06
Raza Akmal <razaakmal11@gmail.com>
2022-12-06 21:44:05 +0500

    design changes for select-pagination

--- src/components/SelectMenu.jsx
+++ src/components/SelectMenu.jsx
@@ -12,27 +12,38 @@ const Menu = (props) => {
           ) : (
             <div className="selct-year">
               {props.options.map((item) => {
-                let checkedValue = props.selectProps.value.some((element) => element?.label === item.label)
+                let checkedValue = props.selectProps.value.some(
+                  (element) => element?.label === item.label
+                );
                 return (
-                <span className="fetching">
-                  <Form.Check
-                    checked={checkedValue}
-                    id={item.label}
-                    value={checkedValue}
-                    onChange={() => props.selectProps.onChange(item)}
-                    type="checkbox"
-                  />
-                  <span> {item.value}</span>
-                </span>
-              )})}
+                  <span className="fetching">
+                    <Form.Check
+                      checked={checkedValue}
+                      id={item.label}
+                      value={checkedValue}
+                      onChange={() => props.selectProps.onChange(item)}
+                      type="checkbox"
+                    />
+                    <span> {item.value}</span>
+                  </span>
+                );
+              })}
             </div>
           )}
-          <button
-            className={"change-data"}
-            onClick={props.selectProps.changeOptionsData}
-          >
-            Change data
-          </button>
+          <div className="carsual-button">
+            <button
+              className={"change-data"}
+              onClick={props.selectProps.changeOptionsData}
+            >
+              <i class="fa fa-arrow-left"></i>
+            </button>
+            <button
+              className={"change-data"}
+              onClick={props.selectProps.changeOptionsData}
+            >
+              <i class="fa fa-arrow-right"></i>
+            </button>
+          </div>
         </div>
       </components.Menu>
     </>
