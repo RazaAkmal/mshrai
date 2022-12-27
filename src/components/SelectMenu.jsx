5fcdaa96a13b2cf9ddf7a74a3044baff7fe24670
Raza Akmal <razaakmal11@gmail.com>
2022-12-27 17:52:37 +0500

    year select pagination done

--- src/components/SelectMenu.jsx
+++ src/components/SelectMenu.jsx
@@ -1,7 +1,58 @@
 import { components } from "react-select";
 import { Form } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
 const Menu = (props) => {
+  const isEnglish = localStorage.getItem("lang") === "en";
+  const [pageonyear, setPageOnYear] = useState(1)
+  const [pageLength, setPageLength] = useState(16)
+  const [totalpages, setTotalPages] = useState(16)
+  const endpage = Math.ceil(props.options.length/totalpages);
+
+  const getWindowWidth = () => {
+    const { innerWidth: width } = window;
+    return width;
+  }
+  const [windowwidth, setWindowWidth] = useState(getWindowWidth());
+
+  
+    useEffect(() => {
+      const handleResize = () => {
+        setWindowWidth(getWindowWidth());
+      }
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+
+    useEffect(() => {
+      if (windowwidth >= 992) {
+        setTotalPages(16)
+        setPageLength(16)
+      } else if (windowwidth >= 768){
+        setTotalPages(12)
+        setPageLength(12)
+      } else if (windowwidth >= 576){
+        setPageLength(8)
+        setTotalPages(10)
+      }
+    }, [windowwidth])
+    
+
+
+  
+  const prevPage = (e) => {
+    setPageOnYear(pageonyear - 1)
+    setPageLength(pageLength / 2)
+    e.preventDefault();
+  }
+  const nextPage = (e) => {
+    setPageOnYear(pageonyear + 1)
+    setPageLength(pageLength * 2)
+    e.preventDefault()
+  }
+
+
 
   return (
     <>
@@ -11,21 +62,27 @@ const Menu = (props) => {
             <span className="fetching">Fetching data...</span>
           ) : (
             <div className="selct-year">
-              {props.options.map((item) => {
+              {props.options.map((item, index) => {
                 let checkedValue = props.selectProps.value.some(
                   (element) => element?.label === item.label
                 );
                 return (
+                  <>
+                  { 
+                  ((pageonyear === 1 && index < pageLength) || (pageonyear > 1 && index >= (pageLength / 2) && index < pageLength)) && 
                   <span className="fetching">
                     <Form.Check
                       checked={checkedValue}
+                      scrollable={true}
                       id={item.label}
                       value={checkedValue}
                       onChange={() => props.selectProps.onChange(item)}
                       type="checkbox"
                     />
                     <span> {item.value}</span>
-                  </span>
+                  </span> }
+
+                  </>
                 );
               })}
             </div>
@@ -33,15 +90,18 @@ const Menu = (props) => {
           <div className="carsual-button">
             <button
               className={"change-data"}
-              onClick={props.selectProps.changeOptionsData}
+              onClick={prevPage}
+              disabled={ pageonyear === 1 }  
             >
-              <i class="fa fa-arrow-left"></i>
+              {isEnglish ? <i class="fa fa-arrow-left"></i> : <i class="fa fa-arrow-right"></i>}
             </button>
             <button
               className={"change-data"}
-              onClick={props.selectProps.changeOptionsData}
+              onClick={nextPage}
+              disabled={pageonyear === endpage }
             >
-              <i class="fa fa-arrow-right"></i>
+              {isEnglish ? <i class="fa fa-arrow-right"></i> : <i class="fa fa-arrow-left"></i>}
+              {/* <i class="fa fa-arrow-right"></i> */}
             </button>
           </div>
         </div>
