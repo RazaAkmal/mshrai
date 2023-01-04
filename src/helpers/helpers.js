000858e4ff863316083d3ed6a1abbb7f0b5e0c78
Raza Akmal <razaakmal11@gmail.com>
2023-01-04 20:50:00 +0500

    show subscribe model after 3 min and display weeks on card

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -40,7 +40,15 @@ export const updateMomentLocaleToArabic = () => {
       h: "أ ساعة",
       hh: "%d ساعات",
       d: "يوم",
-      dd: "%d أيام",
+      dd: function(number) {
+        if (number < 7) {
+          return number + ' أيام'; // Moment uses "d" when it's just 1 day.
+        }
+        else {
+          const weeks = Math.round(number / 7);
+          return weeks + ' ' + (weeks > 1 ? 'أسابيع' : 'أسبوع');
+        }
+      },
       M: "شهر",
       MM: "%d شهر",
       y: "سنة",
@@ -60,9 +68,15 @@ export const updateMomentLocaleToEng = () => {
         h:  "%d hr",
         hh: "%d hrs",
         d:  "%d d",
-        dd: "%d d",
-        w: "%d W",
-        ww: '%d w',
+        dd: function(number) {
+          if (number < 7) {
+            return number + ' days'; // Moment uses "d" when it's just 1 day.
+          }
+          else {
+            const weeks = Math.round(number / 7);
+            return weeks + ' ' + (weeks > 1 ? 'w' : 'W');
+          }
+        },
         M:  "%d mth",
         MM: "%d mths",
         y:  "y",
